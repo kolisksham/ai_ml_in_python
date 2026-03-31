4b527a0e4657e36a62211ed549be3a7d4c600db7
kolisksham <kolisksham@gmail.com>
2026-04-01 02:19:22 +0530

    Day 2: Visualized hiring patterns using matplotlib
    - Created scatter plot for Skill vs Interview scores
    - Added color mapping based on hiring outcome
    - Analyzed data distribution and patterns
    - Compared average scores for hired vs non-hired candidates
    - Gained intuition behind model decision boundaries

---
+++ day02/visualize.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"C:\Users\hp\Desktop\ai_ml_in_python\ai_ml_in_python\datasets\recruitment.csv")
+
+print(f"Hired:\n{df["Hired"].value_counts()}")
+print("_"*100)
+
+colors = df["Hired"]
+
+plt.scatter(df["Skill_Score"], df["Interview_Score"], c=colors)
+plt.xlabel("Skills Score")
+plt.ylabel("Interview Score")
+plt.title("Hiring Selection Graph")
+plt.show()
+
+print(f"Averages:\n{df.groupby('Hired')[['Skill_Score', 'Interview_Score']].mean()}")
